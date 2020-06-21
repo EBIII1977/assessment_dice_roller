583fa533d2172b508817c721f0261f3867bba643
Eugene Bridges III <ebiii@hotmail.com>
2020-06-21 10:03:02 -0500

    created list item tag in JS

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const user_Input = document.querySelector("#user-input")
 const roll_Button = document.querySelector("#roll-button")
 const sumOfRolls = document.querySelector("#sum-of-rolls")
-const ShowAllRoll = document.querySelector("Show-All-Roll")
+const ShowAllRoll = document.querySelector("#Show-All-Roll")
+const rollList = document.querySelector("#roll-list")
+const allRollsList = document.querySelector("#all-Rolls-List")
+
 let dieRolls = 0
 let diceRollArray = []
 let addedDice = 0
@@ -31,10 +34,23 @@ console.log(addedDice)
 // Add sum to the Total area on the page
 document.getElementById("sum-of-rolls").innerHTML = addedDice
 
-// Show all rolls
-
-document.getElementById("Show-All-Roll").innerHTML = diceRollArray
 })
 
+// Show all rolls
+    // document.getElementById("Show-All-Roll").innerHTML = diceRollArray   
+    // document.getElementById("rollList").innerHTML = diceRollArray
 
-
+ShowAllRoll.addEventListener('click', function(){
+    console.log('ShowAllRolls Button Pressed')
+    
+    
+    for (let index = 0; index < diceRollArray.length; index++) {
+        const roll = diceRollArray[index]
+        const newDiceString = '<li class= "dice">' + roll + "</li>"
+        allRollsList.innerHTML += newDiceString
+        
+        console.log(roll)
+        
+    }
+   
+})
